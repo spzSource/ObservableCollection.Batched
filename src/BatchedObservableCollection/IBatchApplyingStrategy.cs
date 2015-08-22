0c39228a18401d370e4930ed308757c277f2883d
Alex Popitich <sasha.popitich@gmail.com>
2015-08-22 20:14:43 +0300

    Added doc-strings for public interfaces

--- src/BatchedObservableCollection/IBatchApplyingStrategy.cs
+++ src/BatchedObservableCollection/IBatchApplyingStrategy.cs
@@ -2,8 +2,18 @@
 
 namespace BatchedObservableCollection
 {
+    /// <summary>
+    /// Provides a logic that determines how batch will be applied
+    /// to current <see cref="IBatchContext{T}"/> context.
+    /// </summary>
+    /// <typeparam name="T">The type of items.</typeparam>
     public interface IBatchApplyingStrategy<T>
     {
+        /// <summary>
+        /// Applies batch <see cref="IList{T}"/> to passed context <see cref="IBatchContext{T}"/>.
+        /// </summary>
+        /// <param name="batchContext">The target context for batch applying.</param>
+        /// <param name="sourceBatch">The items (batch) to be applied.</param>
         void Apply(IBatchContext<T> batchContext, IList<T> sourceBatch);
     }
 }
--- src/BatchedObservableCollection/IBatchContext.cs
+++ src/BatchedObservableCollection/IBatchContext.cs
@@ -1,42 +1,42 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 
 namespace BatchedObservableCollection
 {
     /// <summary>
-    /// 
+    /// The target context for batch.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="T">The type for batch items.</typeparam>
     public interface IBatchContext<T>
     {
         /// <summary>
-        /// 
+        /// The internal collection of already existing elements in context.
         /// </summary>
         IList<T> InternalItems
         {
             get;
-        } 
+        }
 
         /// <summary>
-        /// 
+        /// Prevent reentrancy. See <see cref="ObservableCollection{T}.CheckReentrancy"/> implementation.
         /// </summary>
         void CheckReentrancyInternal();
 
         /// <summary>
-        /// 
+        /// Performs batch applying/
         /// </summary>
-        /// <param name="batch"></param>
-        /// <param name="applyingStrategy"></param>
+        /// <param name="batch">The batch of elements.</param>
+        /// <param name="applyingStrategy">The strategy according to which batch will  be applied.</param>
         void ApplyBatch(IList<T> batch, IBatchApplyingStrategy<T> applyingStrategy);
 
         /// <summary>
-        /// 
+        /// Raises <see cref="ObservableCollection{T}.PropertyChanged"/> event for specified property name.
         /// </summary>
-        /// <param name="args"></param>
         void RaisePropertyChanged(string propertyName);
 
         /// <summary>
-        /// 
+        /// Raises <see cref="ObservableCollection{T}.CollectionChanged"/> event.
         /// </summary>
         void RaiseCollectionChanged(NotifyCollectionChangedEventArgs args);
     }
